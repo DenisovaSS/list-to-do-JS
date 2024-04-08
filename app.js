8f712d941caec3ff28ce6a660d2af80198c35ba3
DenisovaSS <114432399+DenisovaSS@users.noreply.github.com>
2024-04-08 14:38:15 +0300

    add feature add todos, remove todo, change status todo

--- app.js
+++ app.js
@@ -1,23 +1,50 @@
 //Globals
 const todoList = document.getElementById('todo-list');
+const userSelect = document.getElementById('user-todo');
+const form = document.querySelector('form');
 let todos = [];
 let users = [];
 //Attach event
 document.addEventListener('DOMContentLoaded', initApp);
+form.addEventListener('submit', handleSubmit);
 //Basic logic
 function getUserName(userId) {
   const user = users.find((u) => u.id === userId);
   return user.name;
 }
 
-function printTodo({ id, userId, title, complete }) {
+function printTodo({ id, userId, title, completed }) {
   const li = document.createElement('li');
   li.className = 'todo-item';
   li.dataset.id = id;
   li.innerHTML = `<span>${title}  <i>by</i> <b>${getUserName(
     userId
   )}</b></span>`;
+  const status = document.createElement('input');
+  status.type = 'checkbox';
+  status.checked = completed;
+  status.addEventListener('change', handleTodChange);
+  const close = document.createElement('span');
+  close.innerHTML = '&times;';
+  close.className = 'close';
+  close.addEventListener('click', handleClose);
+  li.prepend(status);
+  li.append(close);
   todoList.prepend(li);
+}
+function createUserOption(user) {
+  const option = document.createElement('option');
+  option.value = user.id;
+  option.innerText = user.name;
+  userSelect.append(option);
+}
+function removeTodo(todoId) {
+  todos = todos.filter((todo) => todo.id !== +todoId);
+
+  const todo = todoList.querySelector(`[data-id="${todoId}"]`);
+  todo.querySelector('input').removeEventListener('change', handleTodChange);
+  todo.querySelector('.close').removeEventListener('click', handleClose);
+  todo.remove();
 }
 //Event Logic
 function initApp() {
@@ -27,7 +54,25 @@ function initApp() {
     // console.log(users);
     //отправить в разметку
     todos.forEach((todo) => printTodo(todo));
+    users.forEach((user) => createUserOption(user));
   });
+}
+function handleSubmit(event) {
+  event.preventDefault();
+  createTodo({
+    userId: Number(form.user.value),
+    title: form.todo.value,
+    completed: false
+  });
+}
+function handleTodChange() {
+  const todoId = this.parentElement.dataset.id;
+  const completed = this.checked;
+  toggleTodoComplete(todoId, completed);
+}
+function handleClose() {
+  const todoId = this.parentElement.dataset.id;
+  deleteTodo(todoId);
 }
 //Async logic
 async function getAllTodos() {
@@ -39,4 +84,43 @@ async function getAllUsers() {
   const response = await fetch('https://jsonplaceholder.typicode.com/users');
   const data = await response.json();
   return data;
+}
+async function createTodo(todo) {
+  const response = await fetch('https://jsonplaceholder.typicode.com/todos', {
+    method: 'POST',
+    body: JSON.stringify(todo),
+    headers: { 'Content-Type': 'application/json' }
+  });
+  const newTodo = await response.json();
+  printTodo({ id: newTodo, ...todo });
+}
+async function toggleTodoComplete(todoId, completed) {
+  const response = await fetch(
+    `https://jsonplaceholder.typicode.com/todos/${todoId}`,
+    {
+      method: 'PATCH',
+      body: JSON.stringify({ completed: completed }),
+      headers: { 'Content-Type': 'application/json' }
+    }
+  );
+
+  // const data = await response.json();
+  // console.log(data);
+  if (!response.ok) {
+    //erroe
+  }
+}
+async function deleteTodo(todoId) {
+  const response = await fetch(
+    `https://jsonplaceholder.typicode.com/todos/${todoId}`,
+    {
+      method: 'DELETE',
+      headers: { 'Content-Type': 'application/json' }
+    }
+  );
+  // const data = await response.json();
+  // console.log(data);
+  if (response.ok) {
+    removeTodo(todoId);
+  }
 }
